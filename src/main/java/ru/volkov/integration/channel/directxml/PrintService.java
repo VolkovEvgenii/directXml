092df94202cdb61a1886d6b3e5a2fbab12eb7d24
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-31 15:17:47 +0300

    add publish subscribe channel

--- src/main/java/ru/volkov/integration/channel/directxml/PrintService.java
+++ src/main/java/ru/volkov/integration/channel/directxml/PrintService.java
@@ -5,7 +5,6 @@ import org.springframework.messaging.Message;
 public class PrintService {
 
     public void print(Message<String> message) {
-        throw new RuntimeException("---WooooW---");
-        //System.out.println(message.getPayload());
+        System.out.println(message.getPayload());
     }
 }
